6c8c0640443581383322878a810a0c110f78a74d
François Lépine <francois.lepine@gmail.com>
2019-04-21 15:21:03 -0400

    Ajouter la méthode pour arrondir.

---
+++ Loblaws.Biz/ArrondissementMonnaie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Loblaws.Biz
+{
+    /// <summary>
+    /// Extensions pour l'arrondissement au centime.
+    /// </summary>
+    public static class ArrondissementMonnaie
+    {
+        /// <summary>
+        /// Arrondir au centime près.
+        /// </summary>
+        /// <param name="montant">Montant.</param>
+        /// <returns>Montant arrondi.</returns>
+        public static decimal RoundCurrency(this decimal montant) =>
+            decimal.Round(montant, 2, MidpointRounding.AwayFromZero);
+    }
+}
